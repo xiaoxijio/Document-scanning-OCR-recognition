4ccd27060294cff7d65e40363bcdd9ce9e965ef1
童童大dai王 <97423206+xiaoxijio@users.noreply.github.com>
2024-11-07 10:40:39 +0800

    Update unit.py

--- unit.py
+++ unit.py
@@ -8,22 +8,6 @@ def cv_show(name, img):
     cv2.destroyAllWindows()
 
 
-def sort_contours(cnts, method):
-    reverse = False
-    i = 0
-
-    if method == 'right-to-left' or method == 'bottom-to-top':
-        reverse = True
-
-    if method == 'top-to-bottom' or method == 'bottom-to-top':
-        i = 1
-
-    boundingBoxes = [cv2.boundingRect(c) for c in cnts]  # 外接矩阵
-    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes), key=lambda b: b[1][i], reverse=reverse))  # 打包排序
-
-    return cnts, boundingBoxes
-
-
 def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
     dim = None
     (h, w) = image.shape[:2]
